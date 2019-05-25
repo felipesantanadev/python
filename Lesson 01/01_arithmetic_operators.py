c2909d360bb5729e56483302264455fa525b741d
Felipe Santana <fe.santana@icloud.com>
2019-05-25 01:03:23 -0300

    Arithmetic Operators

---
+++ Lesson 01/01_arithmetic_operators.py
@@ -0,0 +1,24 @@
+# Arithmetic Operators
+
+#   +   (addition)
+#   -   (subtraction)
+#   /   (division)
+#   *   (multiplication)
+#   %   (mod -> rest of division)
+#   **  (exponentiation)
+#   //   (divides two numbers and rounds down the result to the nearest integer)
+
+print(2 + 2)
+print(7 - 2)
+print(10 / 2)
+print(2 * 3)
+print(17 % 4)
+print(2 ** 2)
+print(7 // 2)
+
+# Python also follow the mathematics rules about the oder of operations
+# This will first sum the numbers and multiply after
+print((2 + 4 + 6) * 2)
+
+# This will first multiply 6*2 and sum the result of the multiplication with 2 + 4
+print(2 + 4 + 6 * 2)
